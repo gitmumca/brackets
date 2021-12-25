6a5468d863ab37fe320c6649959a675ff98884a3
Git202008 <mumca@mail.ru>
2021-12-26 01:43:24 +0300

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	
+	arr = bracketsConfig.reduce(function (arr, el) {arr.push(el[0]+el[1]); return arr}, []);
+
+	let nStr, spec = '()[]{}||';
+	while (true) {
+		nStr = str;
+		arr.forEach((el) =>	str = str.replace(new RegExp(((spec.includes(el[0]))?'\\':'')+`${el[0]}`+((spec.includes(el[1]))?'\\':'')+`${el[1]}`, 'gi'), ''));
+		if (nStr == str) {break;}
+	}
+
+	return str.length == 0;
 }
